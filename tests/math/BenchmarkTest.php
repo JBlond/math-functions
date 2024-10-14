2aa72363238038ae457f5f98ff4232ec130241b4
JBlond <leet31337@web.de>
2024-10-14 14:56:56 +0200

    Add benchmark Class tests

---
+++ tests/math/BenchmarkTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace jblond\math;
+
+use PHPUnit\Framework\TestCase;
+
+class BenchmarkTest extends TestCase
+{
+
+    private Benchmark $benchmark;
+
+    public function getProperty($object, $property)
+    {
+        $reflectedClass = new \ReflectionClass($object);
+        $reflection = $reflectedClass->getProperty($property);
+        $reflection->setAccessible(true);
+        return $reflection->getValue($object);
+    }
+    public function setUp(): void
+    {
+        $this->benchmark = new Benchmark();
+    }
+
+    public function testTimerStop():void
+    {
+        $this->benchmark->timerStart('test');
+        $this->benchmark->timerStop('test');
+        $value = $this->getProperty($this->benchmark, 'timingStopTimes');
+        $this->assertArrayHasKey('test', $value);
+    }
+
+    public function testTimerStart(): void
+    {
+        $this->benchmark->timerStart('test');
+        $value = $this->getProperty($this->benchmark, 'timingStartTimes');
+        $this->assertArrayHasKey('test', $value);
+    }
+
+    public function testTimerResult(): void
+    {
+        $this->benchmark->timerStart('test');
+        $this->benchmark->timerStop('test');
+        $result = $this->benchmark->timerResult('test');
+        $value = $this->getProperty($this->benchmark, 'timingStartTimes');
+        $this->assertArrayHasKey('test', $value);
+        $this->assertGreaterThan(0, $result);
+
+    }
+
+    public function testTimerResultZero(): void
+    {
+        $this->benchmark->timerStop('test');
+        $this->benchmark->timerResult('test');
+        $this->assertEquals(
+            0,
+            $this->benchmark->timerResult('test')
+        );
+
+    }
+}
